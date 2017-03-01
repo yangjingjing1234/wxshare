b24eb93e752f92d0fbf23db4f3631694b768d451
yjj <yangjignjing@nuagetimes.com>
2017-03-01 19:25:11 +0800

    新增

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # wxshare
+###微信自定义分享图片,title这是我们平时用的最多的功能，[API](https://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html)接口文档写的也很清除，我也附带了一份我所用到的js
+
+* 想要自己搞，首先你要有一微信认证过的[公众号](https://mp.weixin.qq.com/)，个人可以申请个订阅号，企业可以申请服务号，认证每个月300块大洋，否则不享受这块的功能，
+* 然后你就有一个 appid,appsecrect ,前台很简单，给后台当前网页地址传给后台，一些保密性的数据最好放到后台去获取，后台根据appid,appsecrect获取access_token，然后根据这个获取api_ticket ,api_ticket是用于调用微信卡券JS API的临时票据，有效期为7200 秒,这一堆解释不管怎样微信都有给出相应的后台代码，返回以下数据
+
+```
+wx.config()
+//这块初始话设置也是必须的
+timestamp: data.data.timestamp,
+nonceStr: data.data.nonceStr,//可以前后台协议写死都行
+signature: data.data.signature,
+jsApiList: [
+   // 所有要调用的 API 都要加到这个列表中
+            'onMenuShareAppMessage',
+            'onMenuShareTimeline',
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+            'onMenuShareQZone'
+]
+
+//在后台返回数据之后调用即可
+wx.ready()
+
+//调用接口，自定义图片地址,title 即可
+```
+### 其他功能暂未用到，用到以后在增加
---
+++ wxshare.js
@@ -0,0 +1,122 @@
+﻿var share_desc = '聚天下之贤，策天下之号';
+var share_title = '翼龙贷--寻找特别的你';
+var share_link = "http://webfront.eloancn.com/zhaopin/index.html";
+var share_image = "http://webfront.eloancn.com/zhaopin/image/shareimg.png";
+var url=window.location.href;
+var params={
+	'base':'{}',
+	'body':JSON.stringify({
+		'url':url,
+	})
+}
+$.post('http://webfront.eloancn.com/h5/wx/sign',params,function(data){
+	wx.config({
+		debug: false,
+	    appId: 'wx2610a2bb461261ae',
+	    timestamp: data.data.timestamp,
+	    nonceStr: data.data.nonceStr,//可以前后台协议写死都行
+	    signature: data.data.signature,
+	    jsApiList: [
+	       // 所有要调用的 API 都要加到这个列表中
+	                'onMenuShareAppMessage',
+	                'onMenuShareTimeline',
+	                'onMenuShareQQ',
+	                'onMenuShareWeibo',
+	                'onMenuShareQZone'
+	    ]
+	});
+	wx.ready(function () { 
+                // 在这里调用 API
+            wx.onMenuShareAppMessage({
+                title: share_title, // 分享标题
+                desc: share_desc, // 分享描述
+                link: share_link, // 分享链接
+                imgUrl: share_image, // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                    alert('分享分享给朋友成功1');
+
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+
+            wx.onMenuShareTimeline({
+                title: share_title, // 分享标题
+                desc: share_desc, // 分享描述
+                link: share_link, // 分享链接
+                imgUrl: share_image, // 分享图标
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                    alert('分享到朋友圈2');
+
+
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+
+
+                }
+            });
+            wx.onMenuShareQZone({
+			    title: share_title, // 分享标题
+			    desc: share_desc, // 分享描述
+			    link : share_link,
+				imgUrl: share_image, // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			    	 alert('分享成功');
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    	alert('已取消');
+			    }
+			});
+			wx.onMenuShareQQ({
+			    title: share_title, // 分享标题
+			    desc: share_desc, // 分享描述
+			    link : share_link,
+				imgUrl: share_image, // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			    	 alert('分享成功');
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    	alert('已取消');
+			    }
+			});
+			wx.onMenuShareWeibo({
+			    title: share_title, // 分享标题
+			    desc: share_desc, // 分享描述
+			    link : share_link,
+				imgUrl: share_image, // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			    	 alert('分享成功');
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    	alert('已取消');
+			    }
+			});
+  });
+	wx.checkJsApi({
+	    jsApiList: ['onMenuShareAppMessage',
+	                'onMenuShareTimeline',
+	                'onMenuShareQQ',
+	                'onMenuShareWeibo',
+	                'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+	    success: function(res) {
+	        // 以键值对的形式返回，可用的api值true，不可用为false
+	        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+	        console.log(res)
+	    }
+	});
+	wx.error(function(res){
+		console.log(res);
+	})
+});
